3932e7a055e3fac534a55fee34f255180f437178
kinopico <zqmillet@qq.com>
2023-02-01 21:29:23 +0800

    add some test cases.

--- testcases/encode_test.py
+++ testcases/encode_test.py
@@ -1,13 +1,35 @@
 from textwrap import dedent
 
+from pytest import mark
+
 from sphinxcontrib.plantuml.encode import encode
 
-def test_encode():
-    text = dedent(
-        '''
-        Alice -> Bob: Authentication Request
-        Bob --> Alice: Authentication Response
-        '''
-    ).strip()
-
-    assert encode(text) == 'Syp9J4vLqBLJSCfFib9mB2t9ICqhoKnEBCdCprC8IYqiJIqkuGBAAUW2rJY256DHLLoGdrUS2W0='
+@mark.parametrize(
+    'text, encoded_text', [
+        (
+            dedent(
+                '''
+                Alice -> Bob: Authentication Request
+                Bob --> Alice: Authentication Response
+                '''
+            ).strip(),
+            'Syp9J4vLqBLJSCfFib9mB2t9ICqhoKnEBCdCprC8IYqiJIqkuGBAAUW2rJY256DHLLoGdrUS2W0='
+        ),
+        (
+            dedent(
+                '''
+                @startuml
+                Alice->Bob : I am using hex
+                @enduml
+                '''
+            ).strip(),
+            'SoWkIImgAStDuNBCoKnErRLpoazIi5BmL4ZCLIWjpinBLyX8hU1oICrB0Qe100=='
+        ),
+        (
+            '',
+            '0m0='
+        )
+    ]
+)
+def test_encode(text, encoded_text):
+    assert encode(text) == encoded_text
